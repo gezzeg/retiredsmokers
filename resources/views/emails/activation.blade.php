aa5123ae6030361e764d1321649b2686d5f5501b
gezzeg <mr.ghazali@gmail.com>
2017-07-23 14:55:34 +0800

    Modiefied activation view

--- resources/views/emails/activation.blade.php
+++ resources/views/emails/activation.blade.php
@@ -21,5 +21,4 @@ Please click the following link to activate your account
 {{-- <a href="{{ env('APP_URL')}}/retiredsmokers/public/activation/{{$user->email}}/{{$activationCode}}" title="">Activate Account</a> --}}
 
 <a href="{{ env('APP_URL')}}/activation/{{$user->email}}/{{$activationCode}}" title="">Activate Account</a>
-
 </p>
