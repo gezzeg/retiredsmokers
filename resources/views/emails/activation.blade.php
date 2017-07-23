3ca20634aa9caaa1a4f098aa88a343ee495a0108
gezzeg <mr.ghazali@gmail.com>
2017-07-23 15:33:46 +0800

    remove test.html edit activation.blade

--- resources/views/emails/activation.blade.php
+++ resources/views/emails/activation.blade.php
@@ -11,7 +11,7 @@
 <h1>Hello {{$user->first_name}}</h1>
 
 <p>
-	We have received a request to activate RetiredSmokers account associated with your email address.
+	Thank you. We have received a request to activate RetiredSmokers account associated with your email address.
 </p>
 
 <p>
@@ -20,5 +20,6 @@ Please click the following link to activate your account
 
 {{-- <a href="{{ env('APP_URL')}}/retiredsmokers/public/activation/{{$user->email}}/{{$activationCode}}" title="">Activate Account</a> --}}
 
-<a href="{{ env('APP_URL')}}/activation/{{$user->email}}/{{$activationCode}}" title="">Activate Account</a>
+	<a href="{{ env('APP_URL')}}/activation/{{$user->email}}/{{$activationCode}}" title="">Activate Account</a>
+
 </p>
